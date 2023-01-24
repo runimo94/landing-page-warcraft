f6cf208707b44febebcd785fe43458e61733fde0
runimo94 <runimo94git@gmail.com>
2023-01-24 14:04:25 +0100

    cambios background

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        wall1: "url(assets/background/warcraft_wall_1.png)",
+        wall1xl: "url(assets/background/warcraft_wall_1_xl.png)",
+        wall2: "url(assets/background/warcraft_wall_2.png)",
+      },
+    },
   },
   plugins: [],
 };
