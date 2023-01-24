7bbba17b3c3f98442af02ff1763ec3680cff8cea
runimo94 <runimo94git@gmail.com>
2023-01-24 12:24:53 +0100

    Footer component funcionando

--- src/app/components/footer/info/info.component.ts
+++ src/app/components/footer/info/info.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FooterService } from '../../../services/footer.service';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.scss']
+  styleUrls: ['./info.component.scss'],
 })
 export class InfoComponent {
+  public urlImgLogo?: string;
+  public tituloInfo?: string;
+  public textoInfo?: string;
 
+  constructor(private footerService: FooterService) {
+    this.urlImgLogo = this.footerService.urlImgInfoLogo;
+    this.tituloInfo = this.footerService.tituloInfo;
+    this.textoInfo = this.footerService.textoInfo;
+  }
 }
--- src/app/components/footer/other-games/other-games.component.ts
+++ src/app/components/footer/other-games/other-games.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { OtherGame } from 'src/app/services/footer.service';
+import { FooterService } from '../../../services/footer.service';
 
 @Component({
   selector: 'app-other-games',
   templateUrl: './other-games.component.html',
-  styleUrls: ['./other-games.component.scss']
+  styleUrls: ['./other-games.component.scss'],
 })
 export class OtherGamesComponent {
+  public titulo?: string;
+  public listGames?: OtherGame[];
 
+  constructor(public footerService: FooterService) {
+    this.titulo = this.footerService.tituloOtherGames;
+    this.listGames = this.footerService.listOtherGames;
+  }
 }
