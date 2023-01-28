197c064790aa2020d15ccbdb4532471f2bc23ba8
runimo94 <runimo94git@gmail.com>
2023-01-28 02:02:33 +0100

    Pantalla movil reordenacion del componente seccion

--- src/app/components/seccion-datos/seccion-datos.component.ts
+++ src/app/components/seccion-datos/seccion-datos.component.ts
@@ -1,13 +1,45 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { fromEvent, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-seccion-datos',
   templateUrl: './seccion-datos.component.html',
   styleUrls: ['./seccion-datos.component.scss'],
 })
-export class SeccionDatosComponent {
+export class SeccionDatosComponent implements OnInit, OnDestroy {
   @Input() public titulo?: string;
   @Input() public texto?: string;
   @Input() public urlImagen?: string;
   @Input() public posLeftImg?: boolean;
+
+  private isMoveToRightImg?: boolean;
+
+  private resizeObservable?: Observable<Event>;
+  private resizeSubscription?: Subscription;
+
+  ngOnInit(): void {
+    this.posLeftImg ? (this.isMoveToRightImg = true) : '';
+    this.moveRightImg(window.innerWidth);
+
+    this.resizeObservable = fromEvent(window, 'resize');
+    this.resizeSubscription = this.resizeObservable.subscribe(
+      ({ target }: any) => {
+        this.moveRightImg(target?.innerWidth);
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.resizeSubscription?.unsubscribe();
+  }
+
+  moveRightImg(width: number) {
+    if (this.isMoveToRightImg) {
+      if (width < 768) {
+        this.posLeftImg = false;
+      } else {
+        this.posLeftImg = true;
+      }
+    }
+  }
 }
